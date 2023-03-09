0ae9477e14405d6d28194f905715fcae72ba1ed2
Le Viet Sanh <levietsanh123@gmail.com>
2023-03-09 16:29:59 +0700

    05 Client login and register part 7-end

--- DatingApp/client/src/app/nav/nav.component.ts
+++ DatingApp/client/src/app/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../_models/user';
 import { AccountService } from './../_services/account.service';
 
 @Component({
@@ -8,17 +10,22 @@ import { AccountService } from './../_services/account.service';
 })
 export class NavComponent implements OnInit {
     model: any = {}
-    loggedIn: boolean = false;
-    constructor( private accountService: AccountService) { }
+    // currentUser$: Observable<User>;
+    constructor( public accountService: AccountService) { }
     ngOnInit(): void {
-        
+        // this.currentUser$ = this.accountService.currentUser$;
     }
     login(){
       this.accountService.login(this.model).subscribe(response =>{
         console.log(response);
-        this.loggedIn = true;
       }, error =>{
         console.log(error);
       })
     }
+    logout(){
+      this.accountService.logout();
+      
+    }
+
+   
 }
